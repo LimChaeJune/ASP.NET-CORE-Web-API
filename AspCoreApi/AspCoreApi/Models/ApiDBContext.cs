dc7e74c42cbdfa2b8d9402ad16f80bb027b924e6
DESKTOP-GA0G4OG\cowns <cownsgg@outlook.com>
2020-12-29 20:47:22 +0900

    add project

---
+++ AspCoreApi/AspCoreApi/Models/ApiDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspCoreApi.Models
+{
+    public class ApiDBContext : DbContext
+    { 
+        public ApiDBContext(DbContextOptions<ApiDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<TodoItem> TodoItem { get; set; }
+    }
+}
