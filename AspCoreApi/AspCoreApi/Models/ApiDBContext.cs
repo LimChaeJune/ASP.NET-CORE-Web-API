ffac0a1242f02a3be50d417b3e94e307b1534b3d
DESKTOP-GA0G4OG\cowns <cownsgg@outlook.com>
2021-06-01 16:37:39 +0900

    INIT

---
+++ AspCoreApi/AspCoreApi/Models/ApiDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AspCoreApi.Models
+{
+    public class ApiDBContext : DbContext
+    { 
+        public ApiDBContext(DbContextOptions<ApiDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<TodoItem> TodoItem { get; set; }
+    }
+}
